cd31dc84d73c5e4f8a4783a1f6b19780b3f78a33
GitHub Action <action@github.com>
2022-05-21 06:51:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/ntpdev/ijplugtmpl/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/ntpdev/ijplugtmpl/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.ntpdev.ijplugtmpl.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.ntpdev.ijplugtmpl.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
